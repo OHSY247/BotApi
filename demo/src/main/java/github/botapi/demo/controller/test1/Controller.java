bbef139d238d1594d7307d7f95db5ac618b525a6
straycamel <shenpeng.shenpeng@alibaba-inc.com>
2021-05-28 16:43:24 +0800

    [dev]手动配置多个数据库

---
+++ demo/src/main/java/github/botapi/demo/controller/test1/Controller.java
@@ -0,0 +1,27 @@
+package github.botapi.demo.controller.test1;
+
+import github.botapi.demo.model.DemoDO;
+import github.botapi.demo.service.DemoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * @author straycamel
+ * @date 2021/5/28
+ */
+@RestController
+public class Controller {
+    @Autowired
+    private DemoService demoService;
+    public Controller(DemoService demoService2){
+        demoService = demoService2;
+    }
+    @GetMapping("/test")
+    public List<DemoDO> test1(){
+
+        return demoService.selectAll();
+    }
+}
