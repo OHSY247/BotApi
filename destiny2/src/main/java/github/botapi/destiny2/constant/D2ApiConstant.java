eab86b9c5068e79bdb1a597da97a94bb46d772d3
StrayCamel247 <aboyinsky@outlook.com>
2021-06-15 20:51:43 +0800

    [dev]LinkedProfilesUrl

--- destiny2/src/main/java/github/botapi/destiny2/constant/D2ApiConstant.java
+++ destiny2/src/main/java/github/botapi/destiny2/constant/D2ApiConstant.java
@@ -9,8 +9,6 @@ import java.util.Map;
  */
 public class D2ApiConstant {
     /**
-     * @author straycamel
-     * @date 2021/6/9
      * destiny2 官方接口基础api常量定义
      */
     public static String ROOT = "https://www.bungie.net";
@@ -20,47 +18,48 @@ public class D2ApiConstant {
     public static String HEADER_AUTH_KEY = "X-API-Key";
     public static String SUCCESS_MSG = "Ok";
     /**
-     * @author straycamel
-     * @date 2021/6/9
      * 命运2版本号存储在本地的key值
      */
     public static String VERSION_FLAG = "DESTINY2_MANIFEST_VERSION";
     /**
-     * @author straycamel
-     * @date 2021/6/15
      * https://www.bungie.net/Platform/Destiny2/SearchDestinyPlayer/-1/娃哈哈店长/
      * 通过名字获取玩家基本信息
      */
     private static final String SEARCHDESTINYPLAYER = BASE + "/Destiny2/SearchDestinyPlayer";
     /**
-     * @author straycamel
-     * @date 2021/6/15https://www.bungie.net/Platform/User/GetMembershipsById/4611686018490704250/-1/
+     * https://www.bungie.net/Platform/User/GetMembershipsById/4611686018490704250/-1/
      * 获取玩家的信息
      */
     private static final String GETMEMBERSHIPSBYID = BASE + "/User/GetMembershipsById";
     /**
-     * @author straycamel
-     * @date 2021/6/15
-     * 获取玩家steamid的数据
+     * 获取玩家steamid的数据--用于解析html纪念性获取
      */
     private static String ProfileUrl = ROOT + "/en/Profile";
+    /**
+     * 获取玩家基础数据--主要为了获取玩家
+     * https://www.bungie.net/Platform/Destiny2/-1/Profile/4611686018490704250/LinkedProfiles/
+     */
+    private static final String LinkedProfilesUrl = BASE+"/Destiny2/&d/Profile/&d/LinkedProfiles/";
+
+    public static String getLinkedProfilesUrl(Long membershipType, Long membershipId) {
+        return String.format(LinkedProfilesUrl, membershipType, membershipId);
+    }
+    public static String getProfileUrl(Long membershipId) {
+        return String.format(ProfileUrl + "/%d", membershipId);
+    }
+
+    public static String getGETMEMBERSHIPSBYID(Long membershipId, int membershipType) {
+        return String.format(GETMEMBERSHIPSBYID + "/%d/%d", membershipId, membershipType);
+    }
+
+    public static String getSEARCHDESTINYPLAYER(String name, int membershipType) {
+        return String.format(SEARCHDESTINYPLAYER + "/%d/%s", membershipType, name);
+    }
 
     public static Map<String, String> getAPIKey() {
         Map<String, String> Headers;
         Headers = new HashMap<>();
         Headers.put(D2ApiConstant.HEADER_AUTH_KEY, "09ab307be6e34bf1b80f403d4e20a9fc");
         return Headers;
-    }
-
-    public static String getSEARCHDESTINYPLAYER(String name, int typeFlag) {
-        return String.format(SEARCHDESTINYPLAYER + "/%d/%s", typeFlag, name);
-    }
-
-    public static String getGETMEMBERSHIPSBYID(Long D2Id, int typeFlag) {
-        return String.format(GETMEMBERSHIPSBYID + "/%d/%d", D2Id, typeFlag);
-    }
-
-    public static String getProfileUrl(Long membershipId) {
-        return String.format(ProfileUrl + "/%d", membershipId);
     }
 }
