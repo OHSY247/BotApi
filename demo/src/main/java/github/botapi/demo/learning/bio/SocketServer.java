764bae07f9f5b037d699bc98f1fcce10bfde7a40
StrayCamel247 <aboyinsky@outlook.com>
2021-06-22 23:29:18 +0800

    [learn]bio/nio

---
+++ demo/src/main/java/github/botapi/demo/learning/bio/SocketServer.java
@@ -0,0 +1,35 @@
+package github.botapi.demo.learning.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * bio 模型demo
+ * @author straycamel
+ * @date 2021/6/22
+ */
+public class SocketServer {
+    public static void main(String[] args) throws IOException{
+        ServerSocket serverSocket = new ServerSocket(9000);
+        // 串行一次只能处理一个链接
+        while(true){
+            System.out.println("waiting ....");
+            // block 阻塞方法
+            Socket clientSocket = serverSocket.accept();
+              System.out.println("客户端有链接");
+            handler(clientSocket);
+        }
+    }
+
+    private static void handler(Socket clientSocket) throws IOException{
+        byte[] bytes = new byte[1024];
+        System.out.println("准备read");
+        int read =clientSocket.getInputStream().read(bytes);
+        System.out.println("read完毕");
+        if (read!=-1){
+            System.out.println("收到客户端数据："+ new String(bytes ,0 ,read));
+        }
+
+    }
+}
